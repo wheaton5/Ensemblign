17995d99aa0c7fa025b134a1668e816b702a5b03
Haynes Heaton <whheaton@gmail.com>
2013-12-07 21:46:43 -0500

    new constructor for HMMAligner, and i hope the correct user is being attributed on github now

--- src/main/java/com/heaton/pairwise/HMMAligner.java
+++ src/main/java/com/heaton/pairwise/HMMAligner.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.biojava3.alignment.template.AbstractPairwiseSequenceAligner;
 import org.biojava3.alignment.template.AlignedSequence.Step;
 import org.biojava3.alignment.template.Aligner;
+import org.biojava3.alignment.template.GapPenalty;
 import org.biojava3.alignment.template.MatrixAligner;
 import org.biojava3.alignment.template.PairwiseSequenceAligner;
 import org.biojava3.alignment.template.PairwiseSequenceScorer;
 import org.biojava3.alignment.template.Scorer;
+import org.biojava3.alignment.template.SubstitutionMatrix;
 import org.biojava3.core.sequence.template.Compound;
 import org.biojava3.core.sequence.template.Sequence;
 
@@ -23,6 +25,20 @@ public class HMMAligner<S extends Sequence<C>,C extends Compound>
 		match_matrix = new double[40][40];
 		gap_a = new double[40][40];
 		gap_b = new double[40][40];
+	}
+	
+	/**
+	 * Prepares an HMMAligner to align a particular sequence and target. For the time being, we will 
+	 * assume that the target and sequence are of comparable sizes and an NxM matrix can fit into 
+	 * memory.
+	 * 
+	 * @param query
+	 * @param target
+	 * @param gapPenalty
+	 * @param subMatrix
+	 */
+	public HMMAligner(S query, S target, GapPenalty gapPenalty, SubstitutionMatrix<C> subMatrix){
+		
 	}
 
 	@Override
